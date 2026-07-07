ebb2b478f3b2af63a0e60ed3a74347855c3f112a
AbdulRehman <abdulrehman2132011@gmail.com>
2026-07-07 18:48:57 +0500

    added exponent operator

--- calculator.js
+++ calculator.js
@@ -12,6 +12,9 @@ else if(calculator.operation === "-"){
 else if(calculator.operation === "*"){
     document.write("Answer is:", calculator.Num1 * calculator.Num2);
 }
+else if(calculator.operation === "**"){
+    document.write("Answer is:", calculator.Num1 ** calculator.Num2);
+}
 else if(calculator.operation === "/") {
     if (calculator.Num2 === 0){
         document.write("Error:cannot divide by 0");
