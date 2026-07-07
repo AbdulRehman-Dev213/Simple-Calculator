0d097cadf96605349f08790a5cfcb263518a3ed6
AbdulRehman <abdulrehman2132011@gmail.com>
2026-07-07 18:37:40 +0500

    made a simple and basic calculator using html and Javascript

---
+++ calculator.js
@@ -0,0 +1,30 @@
+// let a = prompt("enter number");
+// console.log(a)
+// if(a % 2 == 0){
+//    console.log("the number is even")
+// }
+// else {
+//     console.log("number is odd")
+// }
+let calculator= {
+    Num1: Number(prompt("Enter Number 1")),
+    Num2: Number(prompt("Enter Number 2")),
+    operation: prompt(" Enter Operation +, -, *,**, / ")
+}
+if(calculator.operation === "+"){
+    document.write("Answer is:", calculator.Num1 + calculator.Num2);
+}
+else if(calculator.operation === "-"){
+    document.write("Answer is:", calculator.Num1 - calculator.Num2);
+}
+else if(calculator.operation === "*"){
+    document.write("Answer is:", calculator.Num1 * calculator.Num2);
+}
+else if(calculator.operation === "/") {
+    if (calculator.Num2 === 0){
+        document.write("Error:cannot divide by 0");
+    }
+    else{
+    document.write("Answer is:", calculator.Num1 / calculator.Num2);
+    }
+}
